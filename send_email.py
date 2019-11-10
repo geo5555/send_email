3a04b9e187b57fa30fb404b0da1261c78b9750c2
george eleftheriou <canobi2002@yahoo.com>
2019-11-10 03:40:26 +0200

    changed send_email.py

--- send_email.py
+++ send_email.py
@@ -9,13 +9,26 @@ username = "george.smith2024@gmail.com"
 password = "idpinsokjyititqb"
 from_email = username
 recipient = "canobi2002@yahoo.com"
+# msg = "hello"  # like this you have no subject
+
+# the following gives you subject but no To
+# msg = f"Subject: test message\n\n"
+# msg = msg+"""
+
+# this is from python"""
+# in unix you need \r\n
+msg = f"From: {from_email}\r\nTo:{recipient}\r\nSubject: test message\r\n\r\n"
+msg = msg+"""
+
+this is from python"""
+
+
 try:
     email_conn = smtplib.SMTP(host, port)
     email_conn.ehlo()
     email_conn.starttls()
     email_conn.login(username, password)
-    email_conn.sendmail(from_email, recipient,
-                        "Hello there this is an email message")
+    email_conn.sendmail(from_email, recipient, msg)
     email_conn.quit()
 except SMTPAuthenticationError as e:
     print("Could not login:"+str(e))
